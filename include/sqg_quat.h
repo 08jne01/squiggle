ff88a14952fd74f112ba12360850861ca5e8c788
Joshua <joshnel123@gmail.com>
2026-01-17 13:01:26 +0000

    Move Quat Functions
    Move the back to the quat header since already have vec3

--- include/sqg_quat.h
+++ include/sqg_quat.h
@@ -21,14 +21,20 @@ namespace sqg
         return quat<T>{}; // since sqg::quat is already initialised to identity quaternion
     }
 
+    template<concepts::vec3_type V, concepts::read_quat_type Q>
+    SQUIGGLE_INLINE constexpr void vector_component( V& vector, const Q& quaternion )
+    {
+        X(vector,  X(quaternion));
+        Y(vector,  Y(quaternion));
+        Z(vector,  Z(quaternion));
+    }
+
     // Returns vector part of quaternion
     template<concepts::read_quat_type T>
     SQUIGGLE_INLINE constexpr vec3<vec_scalar<T>> vector_component( const T& quaternion )
     {
         vec3<vec_scalar<T>> v;
-        X(v,  X(quaternion));
-        Y(v,  Y(quaternion));
-        Z(v,  Z(quaternion));
+        extract_vector(v, quaternion);
         return v;
     }
 
@@ -57,11 +63,6 @@ namespace sqg
 
         // https://www.euclideanspace.com/maths/algebra/realNormedAlgebra/quaternions/arithmetic/index.htm
         vec_value2<Q1,Q2> q;
-        
-        const auto e = W(q1);
-        const auto f = X(q1);
-        const auto g = Y(q1);
-        const auto h = Z(q1);
 
         W(q,  W(q0) * W(q1) - X(q0) * X(q1) - Y(q0) * Y(q1) - Z(q0) * Z(q1));
         X(q,  W(q0) * X(q1) + X(q0) * W(q1) + Y(q0) * Z(q1) - Z(q0) * Y(q1));
@@ -70,42 +71,107 @@ namespace sqg
         return q;
     }
 
+    template<concepts::quat_type Q, concepts::read_vec3_type V>
+    SQUIGGLE_INLINE constexpr vec_value<V> operator*( const Q& quaternion, const V& vector )
+    {
+        static_assert( std::same_as<vec_scalar<Q>,vec_scalar<V>>, "Scalar type must match for this operation" );
+
+        //https://math.stackexchange.com/questions/4037487/quaternion-sandwich-simplification-gives-me-a-term-too-many
+        // (c^2 - b^2)v + 2(v . b)b + 2c(b x v)
+        using scalar = vec_scalar<Q>;
+        const auto c = W(quaternion);
+        const auto b = vector_component(quaternion); 
+        
+        return 
+            (c * c - mag2(b)) * vector + 
+            scalar{2} * dot(vector, b) * b + 
+            scalar{2} * c * cross(b, vector);
+    }
+
+    template<concepts::quat_type Q>
+    SQUIGGLE_INLINE constexpr void set_rotx(Q& quaternion, vec_scalar<Q> angle )
+    {
+        using scalar = vec_scalar<Q>;
+
+        W(quaternion,  std::cos(angle / scalar{2}));
+        
+        const auto sina2 = std::sin(angle / scalar{2});
+        X(quaternion,  sina2);
+        Y(quaternion,  scalar{0});
+        Z(quaternion,  scalar{0});
+    }
+
+    template<concepts::quat_type Q>
+    SQUIGGLE_INLINE constexpr void set_roty(Q& quaternion, vec_scalar<Q> angle )
+    {
+        using scalar = vec_scalar<Q>;
+        W(quaternion,  std::cos(angle / scalar{2}));
+        
+        const auto sina2 = std::sin(angle / scalar{2});
+        X(quaternion,  scalar{0});
+        Y(quaternion,  sina2);
+        Z(quaternion,  scalar{0});
+    }
+
+    template<concepts::quat_type Q>
+    SQUIGGLE_INLINE constexpr void set_rotz(Q& quaternion, vec_scalar<Q> angle )
+    {
+        using scalar = vec_scalar<Q>;
+        W(quaternion,  std::cos(angle / scalar{2}));
+        
+        const auto sina2 = std::sin(angle / scalar{2});
+        X(quaternion,  scalar{0});
+        Y(quaternion,  scalar{0});
+        Z(quaternion,  sina2);
+    }
+
+    template<concepts::quat_type Q, concepts::read_vec3_type V>
+    SQUIGGLE_INLINE constexpr void set_rot( Q& quaternion, const V& vector, typename vec_traits<Q>::scalar_type angle )
+    {
+        static_assert( std::same_as<vec_scalar<Q>,vec_scalar<V>>, "Scalar type must match for this operation" );
+
+        // https://www.euclideanspace.com/maths/algebra/realNormedAlgebra/quaternions/notations/scalarAndVector/index.htm
+        using scalar = typename vec_traits<Q>::scalar_type;
+        W(quaternion) = std::cos(angle / scalar{2});
+    
+        assert( std::abs(mag(vector) - scalar{1}) < scalar{1.0e-6} );
+
+        // axis should be normalised
+        const auto sina2 = std::sin(angle / scalar{2});
+        X(quaternion) = X(vector) * sina2;
+        Y(quaternion) = Y(vector) * sina2;
+        Z(quaternion) = Z(vector) * sina2;
+    }
+
     template<std::floating_point T>
     SQUIGGLE_INLINE constexpr quat<T> rotx_quat( T angle )
     {   
         quat<T> q;        
-        W(q,  std::cos(angle / T{2}));
-        
-        const auto sina2 = std::sin(angle / T{2});
-        X(q,  sina2);
-        Y(q,  T{0});
-        Z(q,  T{0});
+        set_rotx(q);
         return q;
     }
 
     template<std::floating_point T>
     SQUIGGLE_INLINE constexpr quat<T> roty_quat( T angle )
     {   
-        quat<T> q;        
-        W(q,  std::cos(angle / T{2}));
-        
-        const auto sina2 = std::sin(angle / T{2});
-        X(q,  T{0});
-        Y(q,  sina2);
-        Z(q,  T{0});
+        quat<T> q;       
+        set_roty(q);
         return q;
     }
 
     template<std::floating_point T>
     SQUIGGLE_INLINE constexpr quat<T> rotz_quat( T angle )
     {   
-        quat<T> q;        
-        W(q,  std::cos(angle / T{2}));
-        
-        const auto sina2 = std::sin(angle / T{2});
-        X(q,  T{0});
-        Y(q,  T{0});
-        Z(q,  sina2);
+        quat<T> q;  
+        set_rotz(q); 
+        return q;
+    }
+
+    template<concepts::read_vec3_type V>
+    SQUIGGLE_INLINE constexpr quat<vec_scalar<V>> rot_quat( const V& vector, vec_scalar<V> angle )
+    {
+        quat<vec_scalar<V>> q;
+        set_quat(q, vector, angle);
         return q;
     }
 }
